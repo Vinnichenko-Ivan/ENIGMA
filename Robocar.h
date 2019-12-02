1b7ac1cd33b4c10ed81ac3d79d0e9f703fb4fdf0
Vinnichenko-Ivan <33024117+Vinnichenko-Ivan@users.noreply.github.com>
2019-12-02 16:44:13 +0700

    class colorSensor

--- Robocar.h
+++ Robocar.h
@@ -1,1 +1,16 @@
-
+#define pwmA 10
+#define digital1A 8
+#define digital2A 9
+class colorSensor{
+  public:
+    colorSensor();
+    void getLight();
+    void calibrate(int portIn);
+  private:
+    int port;
+    int calibrateK;
+};
+void colorSensor::calibrate(int portIn){
+  port=portIn;
+  calibrateK=analogRead(port);
+}
