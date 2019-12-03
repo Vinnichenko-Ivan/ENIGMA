7207019235dd3c6731f42a6b16d243fd2ac91184
Vinnichenko-Ivan <33024117+Vinnichenko-Ivan@users.noreply.github.com>
2019-12-03 16:08:37 +0700

    Update Robocar.h

--- Robocar.h
+++ Robocar.h
@@ -18,9 +18,33 @@ void colorSensor::calibrate(int portIn){
   pinMode(port,INPUT);
   calibrateK=analogRead(port);
 }
-// class Motor{
-//   public: 
-//     void setSpeed(int Power);
-//     void calibrate(int minPower);
-//     void
-// };
+class Motor{
+  public: 
+    void setSpeed(int Power);
+    void calibrate(int minPower);
+    void stop();
+  private:
+    int minimalPower;
+};
+void Motor::calibrate(int minPower){
+  minimalPower=minPower;
+}
+void Motor::setSpeed(int Power){
+  if(Power>0){
+    Power>100?Power=100:Power=Power;
+    digitalWrite(digital1A,HIGH);
+    digitalWrite(digital2A,LOW);
+    analogWrite(pwmA,map(Power,0,100,minimalPower,255));
+  }
+  if(Power<0){
+    Power<-100?Power=100:Power=-Power;
+    digitalWrite(digital1A,LOW);
+    digitalWrite(digital2A,HIGH);
+    analogWrite(pwmA,map(Power,0,100,minimalPower,255));
+  }
+}
+void Motor::stop(){
+    digitalWrite(digital1A,LOW);
+    digitalWrite(digital2A,LOW);
+    analogWrite(pwmA,0);
+}
