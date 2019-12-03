1eacfe49e90f27ef26ca9ec87dfdf55963552ca7
Vinnichenko-Ivan <33024117+Vinnichenko-Ivan@users.noreply.github.com>
2019-12-03 15:43:21 +0700

    Update Robocar.h

--- Robocar.h
+++ Robocar.h
@@ -3,15 +3,24 @@
 #define digital2A 9
 class colorSensor{
   public:
-    colorSensor();
-    void getLight();
+   //colorSensor();
+    int getLight();
     void calibrate(int portIn);
   private:
     int port;
     int calibrateK;
 };
+int colorSensor::getLight(){
+  return calibrateK-analogRead(port);
+}
 void colorSensor::calibrate(int portIn){
   port=portIn;
-  pinMode(port,INPUT)
+  pinMode(port,INPUT);
   calibrateK=analogRead(port);
 }
+// class Motor{
+//   public: 
+//     void setSpeed(int Power);
+//     void calibrate(int minPower);
+//     void
+// };
