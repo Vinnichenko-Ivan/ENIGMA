387c1a0ccfeca259ffafde93b22886cfe5055ed7
Vinnichenko-Ivan <33024117+Vinnichenko-Ivan@users.noreply.github.com>
2020-03-04 17:38:37 +0700

    Update groundControll.h
    PID

--- maze/groundControll.h
+++ maze/groundControll.h
@@ -1,11 +1,77 @@
 #include<Servo.h>
 #define DEBUG 1
-Servo dropServo(3);
-bool dropBox(){
+class dropServo
+{
+	private:
+		Servo dropServo;
+	public:
+		bool dropBox();
+		dropServo();
+};
+
+dropServo::dropServo()
+{
+		dropServo.attach(3);
+}
+
+bool dropServo::dropBox()
+{
 	dropServo.write(0);
 	delay(1000);
-	dropServo.Write(90);
+	dropServo.write(90);
 	if(DEBUG){
 		Serial.println("I drop Box");
 	}
+}
+//----------------------------------------------------------------------------------------------------------------//
+
+class PID
+{
+  public:
+    double ki=0,kp=0,kd=0,imax=0;
+    double error,errorOld,errorIntegral,errorDeviante;  
+    unsigned long dt,dn;
+    void calibrate(float kpc,float kic,float kdc,float imaxc);
+    double UI(int error);
+};
+
+void PID::calibrate(float kpc,float kic,float kdc,float imaxc)
+{
+  ki=kic;
+  kp=kpc;
+  kd=kdc;
+  imax=imaxc;
+}
+
+double PID::UI(int error)
+{ 
+  errorIntegral+=error*(millis()-dt);
+  if(errorIntegral>imax){
+    errorIntegral=imax;
+  }
+  if(errorIntegral<-imax){
+    errorIntegral=-imax;
+  }
+  errorDeviante=(error-errorOld);
+  errorOld=error;
+  dt=millis();
+  return error*kp +errorIntegral*ki+errorDeviante*kd;
+}
+//-------------------------------------------------------------------------------------------------------------------//
+
+class moveControll
+{
+	private:
+	public:
+	moveControll();
+};
+
+moveControll::moveControll()
+{
+	pinMode(11,OUTPUT);
+	pinMode(10,OUTPUT);
+	pinMode(9,OUTPUT);
+	pinMode(8,OUTPUT);
+	pinMode(7,OUTPUT);
+	pinMode(6,OUTPUT);
 }
