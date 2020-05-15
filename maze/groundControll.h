6112d5ae57783e20c1a9bb7141d704ae86fc37d3
Vinnichenko-Ivan <33024117+Vinnichenko-Ivan@users.noreply.github.com>
2020-05-15 21:44:51 +0700

    Update groundControll.h

--- maze/groundControll.h
+++ maze/groundControll.h
@@ -1,7 +1,9 @@
 #include<Servo.h>
 #include<string.h>
-//#include<cmath.h>
+
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
 
 
 #define DEBUG 1
@@ -50,7 +52,7 @@ void PID::calibrate(float kpc,float kic,float kdc,float imaxc)
 
 double PID::UI(int error)
 { 
-	errorIntegral+=error*(millis()-dt);
+	errorIntegral+=error;//*(millis()-dt);
 	if(errorIntegral>imax)
 	{
 		errorIntegral=imax;
@@ -124,16 +126,53 @@ void motor::setPower(int power)
 		analogWrite(portC,abs(power));
 	}
 }
+//------------------------------------------------------------------------------------------------------------------------------//
+class US
+{
+	private:
+		int port;
+		char filterType='z';//z-zero filter, n- non filter
+	public:
+		int getDistanse();
+		void attach(int portIn, char filterIn);
+};
+
+void US::attach(int portIn, char filterIn)
+{
+	port=portIn;
+	filterType=filterIn;
+}
+
+int US::getDistanse()
+{
+	long duration, distance;
+	pinMode(port,OUTPUT);
+	digitalWrite(port, LOW);
+	delayMicroseconds(2); 
+	digitalWrite(port, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(port, LOW); 
+	pinMode(port,INPUT);
+	duration = pulseIn(port, HIGH, 30000); 
+	distance = duration / 58;
+	if(filterType='z'&&distance==0)
+	{
+		distance=10000;
+	}
+	return distance;
+} 
 //-------------------------------------------------------------------------------------------------------------------//
 
 class moveControl
 {
 	private:
-		
-		motor leftMotor;
-		motor rightMotor;
+
 		PID turnRegulator;
 		PID goRegulator;
+		US leftUS;
+		US rightUS;
+		US backUS;
+		US forwardUS;
 		char c;
 		String beta;
 		int resetIteration=0;
@@ -141,15 +180,20 @@ class moveControl
 		int targetAzimut=0;
 		int gyroOld=0;
 		int programAzimut=0;
-		
+		int calibrateTempLeft,calibrateTempRight;
 		long timerGyro=0;
 		long millisGyro=0;
 		bool gyroD=0;
 		int azimutControl(int azimut);
 		SoftwareSerial SerialGyro =  SoftwareSerial(12, 4);
-		
+		const int leftTemp=A0,rightTemp=A1;
+		Adafruit_MLX90614 mlx;
+		int lenghtToWall=15;
+		int distanse[4];
 
 	public:
+		motor leftMotor;
+		motor rightMotor;
 		int realAzimut=0;
 		moveControl();
 		void setZeroAzimut(int azimut);
@@ -157,12 +201,59 @@ class moveControl
 		void gyro();
 		void testMotor();
 		void turnTo(int currentAzimut);
+		void goTo(int currentAzimut,int power);
+		float giveTemp(char sensor);
+		int* getDistanse();
+		void motorOff();
 };
 
+void moveControl::motorOff()
+{
+	rightMotor.setPower(0);
+	leftMotor.setPower(0);
+}
+
+int* moveControl::getDistanse()
+{
+	distanse[0]=forwardUS.getDistanse();
+	distanse[1]=rightUS.getDistanse();
+	distanse[2]=backUS.getDistanse();
+	distanse[3]=leftUS.getDistanse();
+	return distanse;
+}
+
+float moveControl::giveTemp(char sensor)
+{
+	mlx.begin();
+	if(sensor=='L')
+	{
+		pinMode(leftTemp,OUTPUT);
+		digitalWrite(leftTemp,1);
+		delayMicroseconds(100);
+		return mlx.readObjectTempC();
+		digitalWrite(leftTemp,0);
+	}
+	pinMode(rightTemp,OUTPUT);
+	digitalWrite(rightTemp,1);
+	delayMicroseconds(100);
+	return mlx.readObjectTempC();
+	digitalWrite(rightTemp,0);
+}
+void moveControl::goTo(int currentAzimut,int power)
+{	
+	//currentAzimut=azimutControl(currentAzimut);
+	// if(abs(currentAzimut-realAzimut)>3)
+	// {
+	// 	turnTo(currentAzimut);
+	// }
+	rightMotor.setPower( -power-goRegulator.UI((realAzimut+540-currentAzimut)%360-180));
+	leftMotor.setPower( -power+goRegulator.UI((realAzimut+540-currentAzimut)%360-180));
+}
 void moveControl::turnTo(int currentAzimut)
 {
-	rightMotor.setPower(-((realAzimut+540-currentAzimut)%360-180)*5);
-	leftMotor.setPower(((realAzimut+540-currentAzimut)%360-180)*5);
+	//currentAzimut=azimutControl(currentAzimut);
+	rightMotor.setPower(-turnRegulator.UI((realAzimut+540-currentAzimut)%360-180));
+	leftMotor.setPower(turnRegulator.UI((realAzimut+540-currentAzimut)%360-180));
 }
 
 void moveControl::testMotor()
@@ -217,50 +308,21 @@ moveControl::moveControl()
 	pinMode(12,INPUT);
 	leftMotor.attach(9,10,11);
 	rightMotor.attach(7,8,6);
-	turnRegulator.calibrate(5,0,0,0);
+	turnRegulator.calibrate(10,0,0,50000);
 	goRegulator.calibrate(5,0,0,0);
+	leftUS.attach(A0,'z');
+	rightUS.attach(A2,'z');
+	backUS.attach(A3,'z');
+	forwardUS.attach(A1,'z');
 	if(DEBUG)
 	{
 		Serial.println("I attach my motor!");
 	}
+	mlx = Adafruit_MLX90614();
 }
 
 void moveControl::gyro(){
-	realAzimut=(int)(floor((float)(pulseIn(12, 1)/10)));
+	realAzimut=(int)(floor((float)(pulseIn(12, 1,15000)/10)));
 }
 //---------------------------------------------------------------------------------------------------------------------//
 
-class US
-{
-	private:
-		int port;
-		char filterType='z';//z-zero filter, n- non filter
-	public:
-		int getDistanse();
-		void attach(int portIn, char filterIn);
-};
-
-void US::attach(int portIn, char filterIn)
-{
-	port=portIn;
-	filterType=filterIn;
-}
-
-int US::getDistanse()
-{
-	long duration, distance;
-	pinMode(port,OUTPUT);
-	digitalWrite(port, LOW);
-	delayMicroseconds(2); 
-	digitalWrite(port, HIGH);
-	delayMicroseconds(10); 
-	digitalWrite(port, LOW); 
-	pinMode(port,INPUT);
-	duration = pulseIn(port, HIGH, 30000); 
-	distance = duration / 58;
-	if(filterType='z'&&distance==0)
-	{
-		distance=10000;
-	}
-	return distance;
-}
